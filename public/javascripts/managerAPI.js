978e3290547871d8802f42dab9dce0f27e88812e
Tú Nguyễn <yuki0035vn@gmail.com>
2020-05-25 21:44:56 +0700

    Complete BE managerapi.ejs

--- public/javascripts/managerAPI.js
+++ public/javascripts/managerAPI.js
@@ -1,6 +1,6 @@
 function ItemAPICompoment(name, endpoint) {
     return ` <tr>
-    <td onclick="setValue('${name}', '${endpoint}')">${name}</td>
+    <td onclick="setValue('${name}', '${endpoint}')" style="cursor: pointer;">${name}</td>
     <td>  #  </td>
     <td>
         <button class="btn btn-success btn-sm">View</button>
@@ -11,35 +11,88 @@ function ItemAPICompoment(name, endpoint) {
 </tr>`
 }
 options = {
-    headers:
-        { Authorization: "Bearer " + token }
+    headers: { Authorization: "Bearer " + token }
 };
 var data = [];
 axios.post(BASE_URL + POST_MANANER_API, data, options)
-    .then(function (response) {
-        //   console.log(response.data.data_response);
-        var data = response.data.data_response;
+    .then(function(response) {
+        data = response.data.data_response;
         data.forEach(element => {
-            $('#apiList').append(ItemAPICompoment(element.table_name, element.endpoint_action));
+            $('#apiList').append(ItemAPICompoment(element.table_name, element._id));
+            // console.log(element);
         });
     })
-    .catch(function (error) {
+    .catch(function(error) {
         console.log(error);
     });
 
-function setValue(name, endpoint) {
-    $("#name").text(name);
-    // $("#getAll").unbind('click');
-    $("#getAll").click(getValueAll(endpoint));
+function setValue(name, id) {
+    $('#title').hide();
 
+    $("#getAll").prop("disabled", false);
+    $("#getbyID").prop("disabled", false);
+    $("#deleteAll").prop("disabled", false);
+    $("#deletebyID").prop("disabled", false);
+
+
+    $("#name").text(name);
+
+    $("#getAll").unbind('click');
+    $("#getbyID").unbind('click');
+
+    var endpoint = data.find(v => v._id == id);
+    $("#getAll").click(() => getValueAll(endpoint.endpoint_action.GET_ALL));
+    $("#getbyID").click(() => getValueByID(endpoint.endpoint_action.GET_BY_ID));
+    $("#deleteAll").click(() => deleteValueAll(endpoint.endpoint_action.DELETE));
+    $("#deletebyID").click(() => deleteValueByID(endpoint.endpoint_action.DELETE));
 }
+
+
 
 function getValueAll(endpoint) {
     axios.post(BASE_URL + endpoint, [], options)
-        .then(function (response) {
-            //   console.log(response.data.data_response);
+        .then(function(response) {
             console.log(response);
-           
+            showButtonwithTable();
+        });
+
+}
+
+function getValueByID(endpoint) {
+    var number = $("#NumberID").val();
+    endpoint = endpoint.replace(":id", number);
+    console.log(endpoint);
+    axios.post(BASE_URL + endpoint, [], options)
+        .then(function(response) {
+            // console.log(response);
+            showButtonwithTable();
         });
 }
 
+function deleteValueAll(endpoint) {
+    axios.post(BASE_URL + endpoint, {
+            "id": -1,
+            "deleteForever": "true"
+        }, options)
+        .then(function(response) {
+            // console.log(response);
+            showButtonwithTable();
+        });
+}
+
+function deleteValueByID(endpoint) {
+    var number = $("#RemoveID").val();
+    axios.post(BASE_URL + endpoint, {
+            "id": number,
+            "deleteForever": "true"
+        }, options)
+        .then(function(response) {
+            console.log(response);
+            console.log("DELETE BY ID SUCCESS");
+            showButtonwithTable();
+        });
+}
+
+function showButtonwithTable() {
+    $('#title').show();
+}
